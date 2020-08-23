4831171e5807584b4b2564bfc349fe5f39f517dc
Valendea Amalia <valendea.amalia@gmail.com>
2020-08-23 17:12:36 +0700

    Updates

---
+++ precache-manifest.5f09093ac1adf1d0ebb39e4b70bb3efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312521e22c27a83e961b3e1d6a4b57dd",
+    "url": "/aliens-stack/index.html"
+  },
+  {
+    "revision": "ec151deb174127d225b2",
+    "url": "/aliens-stack/static/css/main.eedd6985.chunk.css"
+  },
+  {
+    "revision": "5403e59d569e66e5d9db",
+    "url": "/aliens-stack/static/js/2.88f06b00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aliens-stack/static/js/2.88f06b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec151deb174127d225b2",
+    "url": "/aliens-stack/static/js/main.331f6c19.chunk.js"
+  },
+  {
+    "revision": "8c535711c76aef8b3b99",
+    "url": "/aliens-stack/static/js/runtime-main.7089f52b.js"
+  }
+]);
